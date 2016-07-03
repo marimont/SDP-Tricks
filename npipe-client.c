15d853132d4512d5ec46b5e76031b5c977499559
Marilisa <marilisamontemurro@gmail.com>
2016-07-03 16:01:59 +0200

    new stuff

---
+++ npipe-client.c
@@ -0,0 +1,36 @@
+/*just a sample client to show how named pipes can be 
+used to implement a clien-server communication*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+#define MAXBUFL 255
+
+char *prog_name;
+
+int main(int argc, char *argv[]){
+	int fd;
+	char recvline[MAXBUFL];
+			/*blocks until a process opens the write terminal*/
+
+	prog_name = argv[0];
+	fd = open("npipe", O_RDONLY);
+	if(fd < 0){
+		return -1;
+	}
+	
+	bzero(recvline, MAXBUFL);
+	while(read(fd, recvline, MAXBUFL) > 0){
+		fprintf(stdout, "[%s] --- received line: %s\n", prog_name, recvline); fflush(stdout);
+		if(!strcmp(recvline, "exit"))
+			break;	
+		bzero(recvline, MAXBUFL);
+	}
+	close(fd);
+
+	return 0;
+}
---
+++ npipe-server.c
@@ -0,0 +1,46 @@
+/*just a sample server to show how named pipes can be 
+used to implement a clien-server communication*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <errno.h>
+
+#define MAXBUFL 255
+
+char *prog_name;
+
+int main(int argc, char *argv[]){
+
+	int fd;
+	char sendline[MAXBUFL];
+ 						/*17 corresponds to fifo already existing -> mkfifo will fail to creat it*/
+	if( mkfifo("npipe", 0666) < 0 && errno != 17){
+		fprintf(stderr, "[%s] --- cannot create fifo - %s\n", prog_name, strerror(errno));	
+		return -1;
+	}
+
+	prog_name = argv[0];
+			/*blocks until a process opens the read terminal*/
+	fd = open("npipe", O_WRONLY);
+	if(fd < 0){
+		return -1;
+	}
+	fprintf(stdout, "[%s] --- connected to client\n", prog_name);
+	fprintf(stdout, "> ");
+	while(fscanf(stdin, "%s", sendline) != EOF){
+		fprintf(stdout, "[%s] --- sending line: %s\n", prog_name, sendline);
+		write(fd, sendline, strlen(sendline));
+		bzero(sendline, MAXBUFL);
+		fprintf(stdout, "> ");
+	}
+	fprintf(stdout, "[%s] --- sending exit line\n", prog_name);
+	write(fd, "exit", 4);
+
+	close(fd);
+
+	return 0;
+}
